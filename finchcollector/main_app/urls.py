c98c296dea6d9f0f47b2c7ab9b3af64397945b4b
User Name <redd@Gilbertos-MBP.lan>
2024-01-10 13:17:38 -0600

    Pt2 Complete

--- finchcollector/main_app/urls.py
+++ finchcollector/main_app/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('finches/', views.finches_index, name='finches')
-   
+    path('finches/', views.finches_index, name='index'),
+    path('finches/<int:finch_id>/', views.finches_detail, name='detail'),
 ]
+
+
+
--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-finches = [
-  {'name': 'Lolo', 'breed': 'Society Finch', 'description': 'skittish', 'age': 1},
-  {'name': 'Sachi', 'breed': 'Purple Finch', 'description': 'gentle and loving', 'age': 2},
-]
+from .models import Finch
+
+# finches = [
+#   {'name': 'Lolo', 'breed': 'Society Finch', 'description': 'skittish', 'age': 1},
+#   {'name': 'Sachi', 'breed': 'Purple Finch', 'description': 'gentle and loving', 'age': 2},
+#   {'name': 'Penny', 'breed': 'Zebra finch', 'description': 'always curious', 'age': 2},
+# ]
 
 
 # Create your views here.
@@ -15,7 +18,23 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+# def finches_index(request):
+#     finches = Finch.objects.all()
+#     return render(request, 'finches/index.html', {
+#         'finches': finches
+#     })
+
 def finches_index(request):
-    return render(request, 'finches/index.html', {
+    finches = Finch.objects.all() # Retrieve all cats
+    return render(request, 'finches/index.html', 
+    { 
         'finches': finches
-    })
+    }
+    )
+# def finches_detail(request, finch_id):
+#     finch = Finch.objects.get(id=finch_id)
+#     return render(request 'finches/detail.html', { 'finch': finch})
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
