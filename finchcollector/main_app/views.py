895d50c7a48cb90c9c0b62e3e6e2c401f4372d12
User Name <redd@Gilbertos-MBP.lan>
2024-01-11 14:12:08 -0600

    Pt3 Completed

--- finchcollector/main_app/views.py
+++ finchcollector/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView, UpdateView, DeleteView
 from .models import Finch
 
 # finches = [
@@ -18,11 +18,7 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-# def finches_index(request):
-#     finches = Finch.objects.all()
-#     return render(request, 'finches/index.html', {
-#         'finches': finches
-#     })
+
 
 def finches_index(request):
     finches = Finch.objects.all() # Retrieve all cats
@@ -31,10 +27,21 @@ def finches_index(request):
         'finches': finches
     }
     )
-# def finches_detail(request, finch_id):
-#     finch = Finch.objects.get(id=finch_id)
-#     return render(request 'finches/detail.html', { 'finch': finch})
 
 def finches_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   return render(request, 'finches/detail.html', { 'finch': finch })
+
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
